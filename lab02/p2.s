f4f02dac0c14f178a43ad5b7cd4d139596d07ba0
joseph1027 <joseph102712080104@gmail.com>
2016-10-21 23:10:22 +0800

    Update p2.s

--- lab02/p2.s
+++ lab02/p2.s
@@ -5,28 +5,37 @@
 	.equ N,20
 
 fibb:
+	cmp R0,#100
+	bgt outdone
+	cmp R0,#1
+	blt outdone
 	mov R5,#2
 	cmp R0,#3
 	blt done
+
+
 fib:
 	mov R1,R2
-	mov R2,R3
-	add R3,R1,R2
+	mov R2,R4
+	add R4,R1,R2
 	add R5,#1
 	cmp R5,R0
 	bne fib
+
 done:
 	bx lr
-
+outdone:
+	ldr R4,=0xFFFFFFFF
+	bx lr
 
 main:
 	movs R0,#N
 	movs R1,#1      @ R1=1
-	movs R2,#1		@ R2=1
-	movs R3,#1      @ R3=1
+	movs R2,#1	@ R2=1
+	movs R4,#1      @ R4=1
 
 	bl fibb
 	ldr R7,=ans
-	str R3,[R7]
+	str R4,[R7]
 
 L: b L
