2f881b231457e87792174044b76addad6df961b7
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2016-10-24 18:33:48 +0800

    New spec.

--- lab02/p2.s
+++ lab02/p2.s
@@ -1,5 +1,10 @@
+	.syntax unified
+	.cpu cortex-m4
+	.thumb
+
 .data
 	ans: .word 1
+
 .text
 	.global main
 	.equ N,20
@@ -9,29 +14,35 @@ fibb:
 	bgt outdone
 	cmp R0,#1
 	blt outdone
-	mov R5,#2
+	mov R5,#2 @count from 2 index
 	cmp R0,#3
 	blt done
 
 fib:
 	mov R1,R2
 	mov R2,R4
-	add R4,R1,R2
+	adds R4,R1,R2
+	bvs overflow
 	add R5,#1
 	cmp R5,R0
 	bne fib
 
 done:
 	bx lr
+
 outdone:
 	ldr R4,=0xFFFFFFFF
 	bx lr
 
+overflow:
+	ldr R4,=0xFFFFFFFE
+	bx lr
+
 main:
-	movs R0,#N
-	movs R1,#1      @ R1=1
-	movs R2,#1	@ R2=1
-	movs R4,#1      @ R4=1
+	mov R0,#N
+	mov R1,#1      @ R1=1
+	mov R2,#1	   @ R2=1
+	mov R4,#1      @ R4=1
 
 	bl fibb
 	ldr R7,=ans
