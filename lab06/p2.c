94759720b132503d73c5131746859318d3ed7cd9
Yu-wen Pwu <ywpu@cs.nctu.edu.tw>
2016-11-28 22:36:14 +0800

    Writing p2.

---
+++ lab06/p2.c
@@ -0,0 +1,101 @@
+#include "stm32l476xx.h"
+
+#define XPORT GPIOC
+#define YPORT GPIOB
+#define X0 GPIO_PIN_0
+#define X1 GPIO_PIN_1
+#define X2 GPIO_PIN_2
+#define X3 GPIO_PIN_3
+#define Y0 GPIO_PIN_6
+#define Y1 GPIO_PIN_5
+#define Y2 GPIO_PIN_4
+#define Y3 GPIO_PIN_3
+
+unsigned int x_pin = {X0, X1, X2, X3};
+unsigned int y_pin = {Y0, Y1, Y2, Y3};
+
+// initial keypad gpio pin, X as output and Y as input
+void keypad_init()
+{
+	RCC->AHB2ENR   |= 0b00000000000000000000000000000110;
+
+	GPIOC->MODER   &= 0b11111111111111111111111100000000;
+	GPIOC->MODER   |= 0b00000000000000000000000001010101;
+	GPIOC->PUPDR   &= 0b11111111111111111111111100000000;
+	GPIOC->PUPDR   |= 0b00000000000000000000000001010101;
+	GPIOC->OSPEEDR &= 0b11111111111111111111111100000000;
+	GPIOC->OSPEEDR |= 0b00000000000000000000000001010101;
+	GPIOC->ODR     |= 0b00000000000000000000000000001111;
+
+	GPIOB->MODER   &= 0b11111111111111111100000000111111;
+	GPIOB->PUPDR   &= 0b11111111111111111100000000111111;
+	GPIOB->PUPDR   |= 0b00000000000000000010101010000000;
+	GPIOB->OSPEEDR &= 0b11111111111111111111111100000000;
+	GPIOB->OSPEEDR |= 0b00000000000000000000000001010101;
+}
+
+/**
+ * scan keypad value
+ *
+ * return:
+ *   >=0: key pressed value
+ *   -1: no key press
+ */
+char keypad_scan()
+{
+	XPORT->BSRR = X0;
+	XPORT->BRR  = X1;
+	XPORT->BRR  = X2;
+	XPORT->BRR  = X3;
+
+	if (GPIO_ReadInputDataBit(YPORT, Y0))
+		return 15;
+	if (GPIO_ReadInputDataBit(YPORT, Y1))
+		return 7;
+	if (GPIO_ReadInputDataBit(YPORT, Y2))
+		return 4;
+	if (GPIO_ReadInputDataBit(YPORT, Y3))
+		return 1;
+
+	XPORT->BRR  = X0;
+	XPORT->BSRR = X1;
+	XPORT->BRR  = X2;
+	XPORT->BRR  = X3;
+
+	if (GPIO_ReadInputDataBit(YPORT, Y0))
+		return 0;
+	if (GPIO_ReadInputDataBit(YPORT, Y1))
+		return 8;
+	if (GPIO_ReadInputDataBit(YPORT, Y2))
+		return 5;
+	if (GPIO_ReadInputDataBit(YPORT, Y3))
+		return 2;
+
+	XPORT->BRR  = X0;
+	XPORT->BRR  = X1;
+	XPORT->BSRR = X2;
+	XPORT->BRR  = X3;
+
+	if (GPIO_ReadInputDataBit(YPORT, Y0))
+		return 14;
+	if (GPIO_ReadInputDataBit(YPORT, Y1))
+		return 9;
+	if (GPIO_ReadInputDataBit(YPORT, Y2))
+		return 6;
+	if (GPIO_ReadInputDataBit(YPORT, Y3))
+		return 3;
+
+	XPORT->BRR  = X0;
+	XPORT->BRR  = X1;
+	XPORT->BRR  = X2;
+	XPORT->BSRR = X3;
+
+	if (GPIO_ReadInputDataBit(YPORT, Y0))
+		return 13;
+	if (GPIO_ReadInputDataBit(YPORT, Y1))
+		return 12;
+	if (GPIO_ReadInputDataBit(YPORT, Y2))
+		return 11;
+	if (GPIO_ReadInputDataBit(YPORT, Y3))
+		return 10;
+}
