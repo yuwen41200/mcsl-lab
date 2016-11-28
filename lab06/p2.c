6b3da3c80f54348640f89a0aebc54fa75e9918a8
joseph1027 <joseph102712080104@gmail.com>
2016-11-29 00:13:24 +0800

    It can compile !!

--- lab06/p2.c
+++ lab06/p2.c
@@ -1,15 +1,16 @@
 #include "stm32l476xx.h"
+#include "utils.h"
 
 #define XPORT GPIOC
 #define YPORT GPIOB
-#define X0 GPIO_PIN_0
-#define X1 GPIO_PIN_1
-#define X2 GPIO_PIN_2
-#define X3 GPIO_PIN_3
-#define Y0 GPIO_PIN_6
-#define Y1 GPIO_PIN_5
-#define Y2 GPIO_PIN_4
-#define Y3 GPIO_PIN_3
+#define X0 GPIO_Pin_0
+#define X1 GPIO_Pin_1
+#define X2 GPIO_Pin_2
+#define X3 GPIO_Pin_3
+#define Y0 GPIO_Pin_6
+#define Y1 GPIO_Pin_5
+#define Y2 GPIO_Pin_4
+#define Y3 GPIO_Pin_3
 
 unsigned int x_pin = {X0, X1, X2, X3};
 unsigned int y_pin = {Y0, Y1, Y2, Y3};
@@ -98,4 +99,15 @@ char keypad_scan()
 		return 11;
 	if (GPIO_ReadInputDataBit(YPORT, Y3))
 		return 10;
+
+	return -1;
+}
+
+int main()
+{
+	gpio_init();
+	max7219_init();
+	keypad_init();
+	char a = keypad_scan();
+	display(a, 2);
 }
---
+++ lab06/utils.h
@@ -0,0 +1,59 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+// these functions are inside the asm file
+extern void gpio_init();
+extern void max7219_init();
+extern void max7219_send(unsigned char address, unsigned char data);
+
+/**
+ * show data on 7-seg via max7219_send
+ *
+ * input:
+ *   data: decimal value
+ *   num_digs: number of digits to show on 7-seg
+ *
+ * return:
+ *   0: success
+ *   -1: illegal data range (out of 8 digits)
+ */
+int display(int data, int num_digs)
+{
+	int data2 = data, i;
+	for (i = 1; i <= num_digs; i++)
+	{
+		if (data % 10 < 0)
+			max7219_send(i, -data % 10);
+		else
+			max7219_send(i, data % 10);
+		data /= 10;
+	}
+	if (data2 < 0)
+		max7219_send(num_digs, 10);
+	for ( ; i <= 8; i++)
+		max7219_send(i, 15);
+	return (data > 99999999 || data < -9999999) ? -1 : 0;
+}
+
+#define GPIO_Pin_0  0b0000000000000001
+#define GPIO_Pin_1  0b0000000000000010
+#define GPIO_Pin_2  0b0000000000000100
+#define GPIO_Pin_3  0b0000000000001000
+#define GPIO_Pin_4  0b0000000000010000
+#define GPIO_Pin_5  0b0000000000100000
+#define GPIO_Pin_6  0b0000000001000000
+#define GPIO_Pin_7  0b0000000010000000
+#define GPIO_Pin_8  0b0000000100000000
+#define GPIO_Pin_9  0b0000001000000000
+#define GPIO_Pin_10 0b0000010000000000
+#define GPIO_Pin_11 0b0000100000000000
+#define GPIO_Pin_12 0b0001000000000000
+#define GPIO_Pin_13 0b0010000000000000
+#define GPIO_Pin_14 0b0100000000000000
+#define GPIO_Pin_15 0b1000000000000000
+
+int GPIO_ReadInputDataBit(GPIO_TypeDef * a, uint16_t b) {
+	return 6;
+}
+
+#endif /* UTILS_H_ */
