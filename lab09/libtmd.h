1f24902d157fd67780a2280493ee04a8fc8cc176
joseph1027 <joseph102712080104@gmail.com>
2016-12-21 18:15:12 +0800

    new

--- lab09/libtmd.h
+++ lab09/libtmd.h
@@ -58,11 +58,11 @@ void TMD_GPIO_SetPinHigh(GPIO_TypeDef *GPIOX, uint16_t GPIO_PIN_Y) {
 
 void usleep(unsigned delay) {
 	RCC->APB1ENR1 |= 0b1;
-	if ((uint32_t) (delay * 0.1) == 0)
-		TIM2->ARR = 1;
+	if (delay == 0)
+		TIM2->ARR = 2;
 	else
-		TIM2->ARR = (uint32_t) (delay * 0.1);
-	TIM2->PSC = (uint32_t) 39999;
+		TIM2->ARR = delay;
+	TIM2->PSC = (uint32_t) 3;
 	TIM2->EGR = TIM_EGR_UG;
 	TIM2->CR1 |= TIM_CR1_CEN;
 	int pre_val = 0;
